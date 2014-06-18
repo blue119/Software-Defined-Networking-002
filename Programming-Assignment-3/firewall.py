028bfecc8a9938295d44691bc7a1070790cebfc1
Yao-Po Wang <blue119@gmail.com>
2014-06-18 20:28:20 +0800

    complete week3

--- Programming-Assignment-3/firewall.py
+++ Programming-Assignment-3/firewall.py
@@ -16,27 +16,32 @@ from collections import namedtuple
 import os
 ''' Add your imports here ... '''
 
-
-
 log = core.getLogger()
-policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ[ 'HOME' ]  
+policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ[ 'HOME' ]
 
 ''' Add your global variables here ... '''
-
-
+FirewallPolicy = {}
+with open(policyFile, 'r') as f:
+    for l in f.readlines()[1:]:
+        fields = l.replace('\n', '').split(',')
+        FirewallPolicy[fields[1]] = fields[2]
 
 class Firewall (EventMixin):
-
     def __init__ (self):
         self.listenTo(core.openflow)
         log.debug("Enabling Firewall Module")
 
-    def _handle_ConnectionUp (self, event):    
+    def _handle_ConnectionUp (self, event):
         ''' Add your logic here ... '''
-        
 
-    
         log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
+        for s, d in FirewallPolicy.items():
+            log.debug("Setup Drop Rule (%s,%s)" % (s, d))
+
+            msg = of.ofp_flow_mod()
+            msg.match.dl_src = EthAddr(s)
+            msg.match.dl_dst = EthAddr(d)
+            event.connection.send(msg)
 
 def launch ():
     '''
